81ec147aa2cf490fec29e8299a7d854da3d525b6
amganier <amganier@users.noreply.github.com>
2017-10-08 10:27:01 -0500

    working around sourcetree auth failure
    Documentation of my failure to utilize lambda and map functions in combination to have the text translation run in an automated fashion against values in a list or dictionary.

--- Assignment1.py
+++ Assignment1.py
@@ -14,10 +14,11 @@ from bs4 import BeautifulSoup  # DOM html manipulation
 # -----------------------------------------------
 
 # Creating a list with the hope of successfully looping through it later 
-# public_emails_list = [
-# "http://www.alumni.northwestern.edu/s/1479/02-naa/16/interior.aspx?sid=1479&gid=2&pgid=25626&cid=42868&ecid=42868&crid=0&calpgid=25618&calcid=42867", 
-# "http://www.alumni.northwestern.edu/s/1479/02-naa/16/interior.aspx?sid=1479&gid=2&pgid=25916&cid=43308&ecid=43308&crid=0&calpgid=25618&calcid=42867"
-# ]
+public_emails_list = [
+ "http://www.alumni.northwestern.edu/s/1479/02-naa/16/interior.aspx?sid=1479&gid=2&pgid=25626&cid=42868&ecid=42868&crid=0&calpgid=25618&calcid=42867", 
+ "http://www.alumni.northwestern.edu/s/1479/02-naa/16/interior.aspx?sid=1479&gid=2&pgid=25916&cid=43308&ecid=43308&crid=0&calpgid=25618&calcid=42867"
+ ]
+ 
 
 # Learning How to Build a loop
 # Citation: Core Python Programming, Second Edition 
@@ -26,12 +27,46 @@ from bs4 import BeautifulSoup  # DOM html manipulation
     
 # Learning how to make a dictionary
 # Citation: Automate the Boring Stuff with Python
-my_emails = {'PurpleLine_20092017':'http://www.alumni.northwestern.edu/?sid=1479&gid=2&pgid=25916&cid=43308&ecid=43308&crid=0&calpgid=25618&calcid=42867','PurpleLine_28082017':'http://www.alumni.northwestern.edu/?sid=1479&gid=2&pgid=25626&cid=42868&ecid=42868&crid=0&calpgid=25618&calcid=42867'}
+# my_emails = {'PurpleLine_20092017':'http://www.alumni.northwestern.edu/?sid=1479&gid=2&pgid=25916&cid=43308&ecid=43308&crid=0&calpgid=25618&calcid=42867','PurpleLine_28082017':'http://www.alumni.northwestern.edu/?sid=1479&gid=2&pgid=25626&cid=42868&ecid=42868&crid=0&calpgid=25618&calcid=42867'}
+
+#u = lambda
+#r = map (requests.get(), public_emails_list)
+
+# attempting to get the transformation to apply to a single URL
+email_url = [http://www.alumni.northwestern.edu/?sid=1479&gid=2&pgid=25916&cid=43308&ecid=43308&crid=0&calpgid=25618&calcid=42867]
+email_name = [PurpleLine_20092017]
+>>> t = ['n'.text]
+>>> h = [html.fromstring('t')]
+>>> map(lambda x,y:x+y, public_emails_list,b)
+[18, 14, 14, 14]
+>>> map(lambda x,y,z:x+y+z, public_emails_list,b,c)
+[17, 10, 19, 23]
+>>> map(lambda x,y,z:x+y-z, public_emails_list,b,c)
+[19, 18, 9, 5]
+
+PurpleLine_20092017 = 
+map(lambda n,u: n = (requests.get('u'), email_name,email_url)
+map(lambda t,n: t = 
 
 
-# Passing item as variable into sequence by using a dictionary
-web_page = requests.get(my_emails['PurpleLine_20092017'], auth=('user', 'pass'))
-# obtain the entire HTML text for the page of interest
+# Insight from conversation with CJG
+# write lambdas that transform data set called on
+# put those into an array in the order I need to call them
+# map once on that array and give it the argument of the data set
+# argument is the data set I want to do it on
+
+
+r = lambda u: requests.get('u')
+t = lambda r: 'r'.text
+h = lambda t: html.fromstring('t')
+b = lambda h: ''.join('h'.xpath('//div[@id="ContentMiddle"]//text()'))
+
+process = array ([r,t,h,b])
+
+web_page_text = web_page.text
+web_page_html = html.fromstring(web_page_text)
+''.join(web_page_html.xpath('//div[@id="ContentMiddle"]//text()'))
+
 
 # show the text including all of the HTML tags... lots of tags
 web_page_text = web_page.text
